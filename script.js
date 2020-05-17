c09c1b903066ab78fa7655c0531b0db790941ed2
Nikola <ristic1900@gmail.com>
2020-05-17 15:54:19 +0200

    hash table

--- script.js
+++ script.js
@@ -80,3 +80,136 @@ const reverse2 = str => {
 }
 
 console.log(revString('Aleksandar'))
+
+
+//merge sorted arrays 
+mergeSortedArrays = (array1, array2) =>  {
+    const mergedArray = [];
+    let array1Item = array1[0]
+    let array2Item = array2[0];
+    let i = 1;
+    let j = 1;
+
+    //Check input 
+    if(array1.length === 0){
+        return array2;
+    }
+    if(array2.length === 0){
+        return array1
+    }
+
+    while(array1Item || array2Item){
+      if(!array2Item || array1Item < array2Item){
+          mergedArray.push(array1Item)
+          array1Item = array1[i];
+          i++;
+      }  else {
+          mergedArray.push(array2Item);
+          array2Item = array2[j];
+          j++
+      }
+    }
+
+    return mergedArray;
+}
+
+console.log(mergeSortedArrays([0,3,4,21], [4,6,30]));
+
+let user = {
+    age: 54,
+    name: 'Nikola',
+    magic: true,
+    scream: function() {
+        console.log('ahhhh');
+    }
+}
+
+user.age // 0(1)
+user.spell = 'abra kadabra'
+user.scream();
+
+const a = new Map() // store value
+const b = new Set() // store key
+
+
+//implement HashTable
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+      // this.data = [];
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+  
+    set(key, value) {
+      let address = this._hash(key);
+      if (!this.data[address]) {
+        this.data[address] = [];
+      }
+      this.data[address].push([key, value]);
+      return this.data;
+    }
+  
+    get(key){
+      const address = this._hash(key);
+      const currentBucket = this.data[address]
+      if (currentBucket) {
+        for(let i = 0; i < currentBucket.length; i++){
+          if(currentBucket[i][0] === key) {
+            return currentBucket[i][1]
+          }
+        }
+      }
+      return undefined;
+    }
+
+    keys() {
+        const keysArray = [];
+        for(let i = 0; i < this.data.length; i++){
+            if(this.data[i]){
+                keysArray.push(this.data[i][0])
+            }
+        }
+        return keysArray;
+    }
+  }
+  
+  const myHashTable = new HashTable(50);
+  console.log(myHashTable.set('grapes', 10000))
+  console.log(myHashTable.get('grapes'))
+  console.log(myHashTable.set('apples', 9))
+  console.log(myHashTable.get('apples'))
+
+
+// first reccuring character
+
+function firstRecurringCharacter(input) {
+    for (let i = 0; i < input.length; i++) {
+      for (let j = i + 1; j < input.length; j++) {
+        if(input[i] === input[j]) {
+          return input[i];
+        }
+      }
+    }
+    return undefined
+  }
+  
+  function firstRecurringCharacter2(input) {
+    let map = {};
+    for (let i = 0; i < input.length; i++) {
+      if (map[input[i]] !== undefined) {
+        return input[i]
+      } else {
+        map[input[i]] = i;
+      }
+    }
+    return undefined
+  }
+  
+console.log(firstRecurringCharacter2([1,5,5,1,3,4,6]))
