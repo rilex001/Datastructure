adecc8a9ff616c244e39aa627fa69a82f18abe08
rilex001 <ristic1900@gmail.com>
2020-06-25 23:43:24 +0200

    fibonacci

--- pripremazaprijemni.js
+++ pripremazaprijemni.js
@@ -139,3 +139,25 @@
 //     }
 // }
 
+//fibonacci 
+
+
+// function fibonacci(num){
+//     var a = 1, b = 0, temp;
+  
+//     while (num >= 0){
+//       temp = a;
+//       a = a + b;
+//       b = temp;
+//       num--;
+//     }
+  
+//     return b;
+//   }
+
+function fibonacci(num) {
+    if (num <= 1) return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+console.log(fibonacci(7))
